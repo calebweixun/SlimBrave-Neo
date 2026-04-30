c6addda7e467f00dcc362a63a26a19be9629a638
calebweixun <calebweixun@gmail.com>
2026-04-30 18:39:32 +0800

    fix: resolve encoding issue in SlimBrave.ps1 by adding Byte Order Mark

--- SlimBrave.ps1
+++ SlimBrave.ps1
@@ -1,4 +1,4 @@
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+﻿if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
     exit
 }
